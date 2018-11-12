3611bf59733665ae41e49ce9c03b895d17ae5d1f
Briant Illescas <BRIANT.ILLESCAS@baruchmail.cuny.edu>
2018-11-12 15:07:06 -0500

    Code finally compiles, added requirements for match function.

--- stocks/stocks/order.cpp
+++ stocks/stocks/order.cpp
@@ -1,5 +1,13 @@
 #include "order.h"
 
+Order::Order() {
+	type = 0;
+	action = 0;
+	price = 0;
+	numShares = 0;
+	ID = "";
+}
+
 istream& operator >> (istream& is, Order* O) {
 	is >> O->type >> O->action >> O->price >>
 		O->numShares >> O->ID;
--- stocks/stocks/orderBook.h
+++ stocks/stocks/orderBook.h
@@ -39,6 +39,8 @@ public:
 	void matchMarket(Order*, int); 
 	void matchLimited(Order*, int); 
 
+	void matchMBid(Order* o);
+
 };
 
 #endif
