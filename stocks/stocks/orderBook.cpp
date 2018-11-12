c02970939e1b672f24e4de13b15000371c68a872
Briant Illescas <BRIANT.ILLESCAS@baruchmail.cuny.edu>
2018-11-12 14:40:13 +0000

    Moved functions from orderBook.h to orderBook.cpp

---
+++ stocks/stocks/orderBook.cpp
@@ -0,0 +1,69 @@
+#include order.h
+
+//definitions for queue functions
+
+template <class T>
+queue<T>::queue()
+{
+	front = nullptr;
+	rear = nullptr;
+	size = 0;
+}
+
+template <class T>
+queue<T>::~queue()
+{
+	clear();
+}
+
+template <class T>
+void queue<T>::enqueue(T *info)
+{
+	queueNode *newNode = nullptr;
+
+	newNode = new queueNode;
+	newNode->p = info;
+	newNode->next = nullptr;
+
+	if (isEmpty())
+	{
+		front = newNode;
+		rear = newNode;
+	}
+	else
+	{
+		rear->next = newNode;
+		rear = newNode;
+	}
+	size++;
+}
+
+template <class T>
+void queue<T>::dequeue(T &info) // not quite sure &info
+{
+	queueNode *temp = nullptr;
+	if (!isEmpty())
+	{
+		info = front->value;
+		temp = front;
+		front = front->next;
+		delete temp;
+		size--;
+	}
+}
+
+template <class T>
+bool queue<T>::isEmpty() const
+{
+	bool status;
+	if (size > 0) status = false;
+	else status = true;
+	return status;
+}
+
+template <class T>
+void queue<T>::clear()    // not quite sure p or *p
+{
+	T p;
+	while (!isEmpty()) dequeue(p);
+}
